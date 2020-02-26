fca1207a145a0eb07e4e07a0283f6d1acccc2475
Bhujay Kumar Bhatta <bhujay.bhatta@itc.in>
2020-02-26 19:52:43 +0530

    only parent exception is able to handle

--- flexflow/domains/xloder/xluploader_exceptions.py
+++ flexflow/domains/xloder/xluploader_exceptions.py
@@ -1,34 +1,35 @@
-import json
+
+from flexflow.exceptions.parent_exception import FlexFlowException
 
 
-class XLUploaderException(Exception):
-    status = "UNKNOWN_STATUS"
-    message = "Unknown status"
-
-    def __init__(self, status=None, message=None):
-        if status:  self.status = status
-        if message: self.message = message
-        self.ret_val = {"status": self.status, "message": self.message }
-        super(XLUploaderException, self).__init__(self.status, self.message)
-
-    def __str__(self):
-        return json.dumps(self.ret_val)
-
-
-    def __repr__(self):
-        return self.ret_val
+# class XLUploaderException(Exception):
+#     status = "UNKNOWN_STATUS"
+#     message = "Unknown status"
+# 
+#     def __init__(self, status=None, message=None):
+#         if status:  self.status = status
+#         if message: self.message = message
+#         self.ret_val = {"status": self.status, "message": self.message }
+#         super(XLUploaderException, self).__init__(self.status, self.message)
+# 
+#     def __str__(self):
+#         return json.dumps(self.ret_val)
+# 
+# 
+#     def __repr__(self):
+#         return self.ret_val
     
-class NoDataExtractedFromExcel(XLUploaderException):
+class NoDataExtractedFromExcel(FlexFlowException):
     status = "NoDataExtractedFromExcel"  
     message = "Conversion from Excel data to a 'None' or Blank  data dictionary " 
     
-class MissingExcelConfig(XLUploaderException):
+class MissingExcelConfig(FlexFlowException):
     status = "MissingExcelConfig" 
     message = "'upload_excel' key and related configs are missing from the config file "
 
         
         
-class InvalidDocCategory(XLUploaderException):
+class InvalidDocCategory(FlexFlowException):
     status = "InvalidDocCategory"    
     def __init__(self, status_doc_category, list_doc_category):
         self.status_doc_category = status_doc_category
